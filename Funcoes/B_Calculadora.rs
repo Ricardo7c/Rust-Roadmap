c460b2d2baf5dc8e561e4a43700a9a355bb826ea
Ricardo7c <Ricardo7c@hotmail.com>
2024-10-31 03:10:03 -0300

    Finalizado!

--- Funcoes/B_Calculadora.rs
+++ Funcoes/B_Calculadora.rs
@@ -1,3 +1,21 @@
 fn main(){
-    
+    let soma = |a, b| a + b;
+    let sub = |a, b| a - b;
+    let multi = |a, b| a * b;
+    let div = |a, b| {
+        if b != 0 {
+            Some(a / b)
+        } else {
+            None
+        }
+    };
+
+
+    println!("{}", soma(5,2));
+    println!("{}", sub(5,2));
+    println!("{}", multi(5,2));
+    match div(5,0) {
+        Some(resultado) => println!("{}", resultado),
+        None => println!("Erro, divisão por zero não é permitida.")
+    }
 }
