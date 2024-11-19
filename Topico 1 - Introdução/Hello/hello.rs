a729870b96512bb189588476922dcc48e722deb3
Ricardo7c <Ricardo7c@hotmail.com>
2024-11-19 00:03:55 -0300

    Finalizado!

--- Topico 1 - Introdução/Hello/hello.rs
+++
@@ -1,6 +0,0 @@
-
-// Função principal, todo programa Rust começa pela main
-fn main() {
-    // Println! é uma macro para exibição no terminal
-    println!("Hello, World"); // Esse programa exibe Hello, World no terminal
-}
