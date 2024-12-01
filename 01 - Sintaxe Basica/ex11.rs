32f15bcdb953859c7d62f6e1ce7fa2b3c45460d4
Ricardo7c <Ricardo7c@hotmail.com>
2024-12-01 12:54:24 -0300

    Finalizado!

---
+++ 01 - Sintaxe Basica/ex11.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let numero = 25;
+    if numero < 10{
+        println!("O numero é pequeno");
+    }else if numero > 10 && numero < 20 {
+        println!("O numero é médio");
+    }else{
+        println!("O numero é maior do que 20");
+    }
+}
