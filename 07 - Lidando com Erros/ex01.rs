4510e4067d3029144a4639d6c5926a682fee8211
Ricardo7c <Ricardo7c@hotmail.com>
2024-12-02 15:08:46 -0300

    Finalizado!

--- 07 - Lidando com Erros/ex01.rs
+++ 07 - Lidando com Erros/ex01.rs
@@ -1,8 +1,17 @@
-fn parse_input(entrada: String) -> Result<i32, String>{
-    let saida = entrada.parse::<i32>()?;
-    Ok(saida)
+use std::fs::File;
+use std::io::Read;
+
+fn read_file(arquivo: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(arquivo)?;
+    let mut conteudo = String::new();
+    file.read_to_string(&mut conteudo)?;
+    Ok(conteudo)
 }
 
-fn main(){
-
+fn main() {
+    let arquivo = "Arquivo.txt";
+    match read_file(arquivo) {
+        Ok(conteudo) => println!("{}", conteudo),
+        Err(x) => println!("{:?}", x)
+    }
 }
